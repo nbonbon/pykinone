8139079b56c61cad73e315e6d0bfe33d8fcc99ea
Nick Bonnet <njbuwm@gmail.com>
2022-11-17 21:21:21 -0500

    Test and Class skelotons

---
+++ Util/TimeUtil.py
@@ -0,0 +1,8 @@
+class TimeUtil:
+    @staticmethod
+    def utcToLocal(utcTime):
+        pass
+
+    @staticmethod
+    def localToUtc(localTime):
+        pass
---
+++ unit_tests/test_TimeUtil.py
@@ -0,0 +1,5 @@
+from Util.TimeUtil import TimeUtil
+
+def test_utcToLocalTime():
+    result = TimeUtil.utcToLocal()
+
