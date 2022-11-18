ee29bdb9dae1421679c2227c1422d8de09e6ab47
Nick Bonnet <njbuwm@gmail.com>
2022-11-18 15:47:19 -0500

    local to utc and vv methods and tests

--- Util/TimeUtil.py
+++ Util/TimeUtil.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+from dateutil import tz
+
 class TimeUtil:
     @staticmethod
-    def utcToLocal(utcTime):
-        pass
+    def utcToLocal(utcTime, to_zone):
+        from_zone = tz.gettz('UTC')
+        utcTime = utcTime.replace(tzinfo=from_zone)
+        return utcTime.astimezone(to_zone)
 
     @staticmethod
-    def localToUtc(localTime):
-        pass
+    def localToUtc(localTime, from_zone):
+        to_zone = tz.gettz('UTC')
+        localTime = localTime.replace(tzinfo=from_zone)
+        return localTime.astimezone(to_zone)
--- unit_tests/test_TimeUtil.py
+++ unit_tests/test_TimeUtil.py
@@ -1,5 +1,36 @@
+from datetime import datetime
+from dateutil import tz
+
 from Util.TimeUtil import TimeUtil
 
-def test_utcToLocalTime():
-    result = TimeUtil.utcToLocal()
+def test_utcToLocal_EST():
+    utc = datetime.fromisoformat("2022-11-18 20:17:43")
+    to_zone = tz.gettz('America/Indiana/Indianapolis') 
 
+    result = TimeUtil.utcToLocal(utc, to_zone)
+
+    assert result.isoformat(" ","seconds") == "2022-11-18 15:17:43-05:00"
+
+def test_utcToLocal_UTCtoUTC():
+    utc = datetime.fromisoformat("2022-11-18 20:17:43")
+    to_zone = tz.gettz('UTC') 
+
+    result = TimeUtil.utcToLocal(utc, to_zone)
+
+    assert result.isoformat(" ","seconds") == "2022-11-18 20:17:43+00:00"
+
+def test_localToUtc_EST():
+    est = datetime.fromisoformat("2022-11-18 15:17:43")
+    from_zone = tz.gettz('America/Indiana/Indianapolis') 
+    
+    result = TimeUtil.localToUtc(est, from_zone)
+
+    assert result.isoformat(" ","seconds") == "2022-11-18 20:17:43+00:00"
+
+def test_localToUtc_UTC():
+    utc = datetime.fromisoformat("2022-11-18 15:17:43")
+    from_zone = tz.gettz('GMT') 
+    
+    result = TimeUtil.localToUtc(utc, from_zone)
+
+    assert result.isoformat(" ","seconds") == "2022-11-18 15:17:43+00:00"
